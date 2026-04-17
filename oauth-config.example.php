615167ad3eddc1ced9bb509e6042a78f1971e003
ms25951394-dhananjaya <ms25951394@my.sliit.lk>
2026-04-17 15:53:27 +0530

    Add OAuth configuration template

---
+++ oauth-config.example.php
@@ -0,0 +1,6 @@
+<?php
+// oauth-config.example.php - TEMPLATE FILE
+$google_client_id = 'YOUR_CLIENT_ID_HERE.apps.googleusercontent.com';
+$google_client_secret = 'YOUR_CLIENT_SECRET_HERE';
+$google_redirect_uri = 'http://localhost/ecom/google-callback.php';
+?>
